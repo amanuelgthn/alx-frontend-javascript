958f5307f2e89ee6a9d6b0a1ae32f24cf3ab642c
amanuelgthn <amanuelgthn@gmail.com>
2023-11-27 18:02:11 +0100

    create 2-arrow.js

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+const newNeighborhoodlist = new getNeighborhoodsList();
+const res = newNeighborhoodlist.addNeighborhood('Noe Valley');
+console.log(res);
