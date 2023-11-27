7ec5612056b251e31bda73d2c950ad4d2ae9cb9c
amanuelgthn <amanuelgthn@gmail.com>
2023-11-27 18:28:19 +0100

    use rest paramater on 4-rest-paramater

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,8 @@
-export default function returnHowManyArguments() {
-  return arguments.length;
+export default function returnHowManyArguments(...args) {
+  let total = 0;
+  /* eslint-disable no-unused-vars */
+  for (const arg of args) {
+    total += 1;
+  }
+  return total;
 }
