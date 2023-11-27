71d5ea8ed7a89f1317f5c4a69840152e3549c9b7
amanuelgthn <amanuelgthn@gmail.com>
2023-11-27 19:00:41 +0100

    Spread syntax concat arrays and string

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,1 +1,2 @@
-export default (array1, array2, string) => array1.concat(array2, ...string);
+export default (array1, array2, string) => [...array1, ...array2, ...string];
+// export default (array1, array2, string) => array1.concat(array2, ...string);
