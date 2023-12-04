9349c4386a1b51a36d3fc20fcc10bcbb6bf19485
amanuelgthn <amanuelgthn@gmail.com>
2023-12-04 17:46:28 +0100

    GEt sum using reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,7 @@
 export default function getStudentIdSum(arr) {
-    
+  const initial = 0;
+  const id = arr.map(({ id }) => (id));
+  const sumwithInitial = id.reduce((accumulator,
+    currentValue) => accumulator + currentValue + initial);
+  return sumwithInitial;
 }
