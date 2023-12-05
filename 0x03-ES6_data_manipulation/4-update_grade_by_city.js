cdfbcc68b6fd3c85a2094845ac8a1e76c799dc87
amanuelgthn <amanuelgthn@gmail.com>
2023-12-05 16:28:08 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-    // getListStudnets a list of students
-    // city string to filter the data
-    // newGrades Array of new grads as parameters for updating the list
+  // getListStudnets a list of students
+  // city string to filter the data
+  // newGrades Array of new grads as parameters for updating the list
 
-      const filteredCity =  getListStudents.filter((x) => x.location === city);
+  const filteredCity = getListStudents.filter((x) => x.location === city);
 
-      const updatedStudents = filteredCity.map((student) => {
-        const filteredGrade = newGrades.filter((grade) => grade.studentId === student.id)[0];
-        let newStudent = { ...student, grade: "N/A" };
-        if (filteredGrade) {
-          newStudent = { ...student, grade: filteredGrade.grade };
-        }
-        return newStudent;
-      });
-      return updatedStudents;
+  const updatedStudents = filteredCity.map((student) => {
+    const filteredGrade = newGrades.filter((grade) => grade.studentId === student.id)[0];
+    let newStudent = { ...student, grade: 'N/A' };
+    if (filteredGrade) {
+      newStudent = { ...student, grade: filteredGrade.grade };
     }
+    return newStudent;
+  });
+  return updatedStudents;
+}
