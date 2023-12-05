1e2297c30da28072c5745dc385fb1e9079fa8845
amanuelgthn <amanuelgthn@gmail.com>
2023-12-05 18:36:20 +0100

    7-has_array_values.js

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,13 @@
+export default function hasValuesFromArray(set, arr) {
+  if (arr.length > set.length) {
+    return false;
+  }
+
+  for (const i of arr) {
+    if (!set.has(i)) {
+      return false;
+    }
+  }
+
+  return true;
+}
