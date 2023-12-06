695dab13d434fd4c4cf27970371fd96eb194d78b
amanuelgthn <amanuelgthn@gmail.com>
2023-12-06 09:39:47 +0100

    eslint fic 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,14 @@
 export default function cleanSet(set, startString) {
-    let result = [];
-    for (const item of set) {
-        if (item.includes(startString)) {
-            result.push(item);
-        }
+  const result = [];
+  if (startString.length < 1) {
+    return '';
+  }
+  for (const item of set) {
+    if (item.length > startString.length) {
+      if (item.slice(0, startString.length) === startString) {
+        result.push(item.slice(startString.length));
+      }
     }
-    return result;
+  }
+  return result.join('-');
 }
