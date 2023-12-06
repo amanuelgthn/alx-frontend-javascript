40f47b556fa9600641879d97a92bc8e6ad907b00
amanuelgthn <amanuelgthn@gmail.com>
2023-12-06 10:24:27 +0100

    Fix input string handler

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,13 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (startString.length < 1) {
+  if (startString.length < 1 && startString === '') {
     return '';
   }
   for (const item of set) {
-    if (item.length > startString.length) {
+    if (item === undefined) {
+        continue;
+    }
+    else if (item.length > startString.length) {
       if (item.slice(0, startString.length) === startString) {
         result.push(item.slice(startString.length));
       }
