cd72b16f2bf48960a9cf633c3e10e400b1919157
amanuelgthn <amanuelgthn@gmail.com>
2023-12-06 11:36:55 +0100

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,10 @@
 export default function updateUniqueItems(map) {
-  map.forEach((val, key) => {
-    if (val === 1) {
-      map.set(key, 100);
-    }
-  });
+  if (map instanceof Map) {
+    map.forEach((val, key) => {
+      if (val === 1) {
+        map.set(key, 100);
+      }
+    });
+  }
   return map;
 }
