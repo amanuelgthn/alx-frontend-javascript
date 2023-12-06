075325a6fbd5960ac591e4235c8ae7f25e3a274f
amanuelgthn <amanuelgthn@gmail.com>
2023-12-06 10:48:42 +0100

    Fix input string handler

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (startString.length < 1 && startString === '') {
+  if (startString === undefined) {
+    return '';
+  }
+  if (startString.length < 1) {
     return '';
   }
   for (const item of set) {
-    if (item === undefined) {
-        continue;
-    }
-    else if (item.length > startString.length) {
+    if (item !== undefined && item.length > startString.length) {
       if (item.slice(0, startString.length) === startString) {
-        result.push(item.slice(startString.length));
+        result.push(item.slice(startString.length).replace('-', ''));
       }
     }
   }
