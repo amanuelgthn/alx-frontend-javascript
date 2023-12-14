c44e43f4b03436dbb1389d8472fae0a1660271a0
amanuelgthn <amanuelgthn@gmail.com>
2023-12-14 15:05:50 +0100

    Task_5

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,33 @@
+export interface MajorCredits {
+    credits: number;
+    brand: "Major";
+}
+
+export interface MinorCredits {
+    credits: number;
+    brand: "Minor";
+}
+
+function sumMajorCredits (
+    subject1: MajorCredits,
+    subject2: MajorCredits
+    ): MajorCredits {
+        const sum = subject1.credits + subject2.credits;
+        const obj: MajorCredits = {
+            credits: sum,
+            brand: "Major",
+        };
+        return obj;
+}
+
+function sumMinorCredits (
+    subject1: MinorCredits,
+    subject2: MinorCredits
+): MinorCredits {
+    const sum = subject1.credits + subject2.credits;
+    const obj: MinorCredits = {
+        credits: sum,
+        brand: "Minor",
+    };
+    return obj;
+}
