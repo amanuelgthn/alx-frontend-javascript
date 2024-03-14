568e4a7b82455c2bbabfcbc60973ec7b09e9930b
amanuelgthn <amanuelgthn@gmail.com>
2024-03-14 13:13:38 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import { uploadPhoto, createUser } from './utilis';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-        .then(([body, fullName]) => console.log(body, fullName))
+        .then(([body, fullName]) => console.log(fullName))
         .catch(() => console.log('Signup system offline'));
 }
