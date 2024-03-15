346353371ebc1e60a932c1632ce79a7744421c5a
amanuelgthn <amanuelgthn@gmail.com>
2024-03-15 19:00:14 +0100

    6-final-user.js eslint fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,30 +1,31 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
-    firstName,
-    lastName,
-    filename) {
-        const result = [];
-        try {
-            const user = await signUpUser(firstName, lastName);
-            result.push({status: 'fulfiled', value: user });
-    } catch (err) {
-        result.push({
-            status: 'rejected',
-            value: err.toString(),
-        });
-    }
+  firstName,
+  lastName,
+  filename,
+) {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfiled', value: user });
+  } catch (err) {
+    result.push({
+      status: 'rejected',
+      value: err.toString(),
+    });
+  }
 
-    try {
-        const upload = await uploadPhoto(filename);
-        result.push({
-            status: 'fulfiled',
-            value: upload });
-        } catch (err) {
-            result.push({status: 'rejected', value: err.toString(),
-        });
-    }
+  try {
+    const upload = await uploadPhoto(filename);
+    result.push({
+      status: 'fulfiled',
+      value: upload,
+    });
+  } catch (err) {
+    result.push({ status: 'rejected', value: err.toString() });
+  }
 
-    return result;
+  return result;
 }
