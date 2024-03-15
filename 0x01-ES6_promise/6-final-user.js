32b65ae39a7b08828141f1ea723727806c8ba4e6
amanuelgthn <amanuelgthn@gmail.com>
2024-03-15 18:41:01 +0100

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,30 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default async function handleProfileSignup(
+    firstName,
+    lastName,
+    filename) {
+        const result = [];
+        try {
+            const user = await signUpUser(firstName, lastName);
+            result.push({status: 'fulfiled', value: user });
+    } catch (err) {
+        result.push({
+            status: 'rejected',
+            value: err.toString(),
+        });
+    }
+
+    try {
+        const upload = await uploadPhoto(filename);
+        result.push({
+            status: 'fulfiled',
+            value: upload });
+        } catch (err) {
+            result.push({status: 'rejected', value: err.toString(),
+        });
+    }
+
+    return result;
+}
