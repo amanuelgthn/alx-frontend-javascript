59c9fcf5af8da3b7d1f38bbb1127f5ec325446f6
amanuelgthn <amanuelgthn@gmail.com>
2024-04-12 10:59:46 +0200

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,12 +2,18 @@ class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) === 'string') {
       this._name = name;
+    } else {
+      throw new TypeError('name must be a string');
     }
     if (typeof (length) === 'number') {
       this._length = length;
+    } else {
+      throw new TypeError('length must be a number');
     }
     if (Array.isArray(students)) {
       this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 
