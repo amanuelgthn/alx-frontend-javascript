26270bb84b1c29b9e4c248d48dcf2fe7347c4740
amanuelgthn <amanuelgthn@gmail.com>
2024-04-12 10:56:11 +0200

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -24,30 +24,26 @@ class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof (name) === 'string')
-    {
+    if (typeof (name) === 'string') {
       this._name = name;
-    }
-    else {
-      throw new Error(Error)
+    } else {
+      throw new TypeError('name must be a string');
     }
   }
 
   set length(length) {
     if (typeof (length) === 'number') {
       this._length = length;
-    }
-    else {
-      throw new Error(Error)
+    } else {
+      throw new TypeError('length must be a number');
     }
   }
 
   set students(students) {
     if (Array.isArray(students)) {
       this._students = students;
-    }
-    else {
-      throw new Error(Error)
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
