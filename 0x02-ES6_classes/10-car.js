e603fb595441b80b296cb7b8930597e4d93c3142
amanuelgthn <amanuelgthn@gmail.com>
2024-04-18 20:27:07 +0200

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,25 @@
+class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand === 'string') {
+      this._brand = brand;
+    } else {
+      throw new Error('brand must be a string');
+    }
+    if (typeof motor === 'string') {
+      this._motor = motor;
+    } else {
+      throw new Error('motor must be a string');
+    }
+    if (typeof color === 'string') {
+      this._color = color;
+    } else {
+      throw new Error('color must be a string');
+    }
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+export default Car;
