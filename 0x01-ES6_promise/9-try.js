73f50ff4e8dffc790eb5134f677c9d6e097ae399
amanuelgthn <amanuelgthn@gmail.com>
2024-05-09 19:30:52 +0200

    fix error message 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (e) {
-    queue.push(e.message);
+    queue.push(`Error: ${e.message}`);
   } finally {
     queue.push('Guardrail was processe');
   }
